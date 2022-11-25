ba54964f83730c5ca63f7f2c0b689b2cdf6f39dd
Tom Hendra <tom.hendra@outlook.com>
2022-11-25 08:46:35 +0100

    refactor: demo of javascript closure understanding

--- helpers/youtube-api.helper.ts
+++ helpers/youtube-api.helper.ts
@@ -1,7 +1,4 @@
 import { sampleOne } from '~/utils/main';
-
-const API = 'https://youtube.googleapis.com/youtube/v3';
-
 /* 
   The YouTube Data API quota is 10K points per day, but every time we call the 
   list method it costs 100 pts - we get 100 calls per day.
@@ -12,7 +9,7 @@ const API = 'https://youtube.googleapis.com/youtube/v3';
   eaten up between builds on Vercel, development and route changes when testing 
   the app.
 
-  My below solution is to use a random API key from 8 different apps created on
+  My solution is to use a random API key from 8 different apps created on
   Google Cloud Platform. This is a bit verbose, and a package does exists that 
   interpolates env vars, but it seems overkill fot this project. 
   
@@ -24,6 +21,8 @@ const API = 'https://youtube.googleapis.com/youtube/v3';
 
   We should use keep an eye on package size: https://bundlephobia.com/ 
 */
+const API = 'https://youtube.googleapis.com/youtube/v3';
+
 const keys = [
   process.env.YOUTUBE_API_KEY_1,
   process.env.YOUTUBE_API_KEY_2,
@@ -34,12 +33,6 @@ const keys = [
   process.env.YOUTUBE_API_KEY_7,
   process.env.YOUTUBE_API_KEY_8,
 ];
-
-const auth = sampleOne(keys);
-
-/* 
-  types guard us against a bad configuration of method calls to the google api. 
-*/
 
 type ChannelsOptions = {
   channelType?: 'any' | 'show';
@@ -63,40 +56,67 @@ type VideoOptions = {
   videoEmbeddable?: 'any' | 'true';
   videoType?: 'any' | 'episode' | 'movie';
 };
-/* 
-  be cautious when adding to this url builder, as not all methods are prepended 
-  with &, for example location requires %2c to represent a comma between 
-  latitude and longitude coords.
-*/
-function getYouTubeApiEndpoint(
-  type: 'channel' | 'video' | 'playlist',
-  options?: ChannelsOptions | VideoOptions,
-) {
-  let url = `${API}/search?part=snippet&type=${type}`;
 
-  if (options) {
-    const { maxResults } = options;
+function getEndpointGenerator() {
+  const auth = sampleOne(keys);
+  /* 
+    be cautious when adding options to the url generator, as not all methods 
+    are prepended with &. for example location requires %2c to represent a 
+    comma in between latitude and longitude values.
+  */
+  function generateEndpointUrl(
+    type: 'channel' | 'video' | 'playlist',
+    options?: ChannelsOptions | VideoOptions,
+  ) {
+    let url = `${API}/search?part=snippet&type=${type}`;
 
-    if (maxResults && maxResults > 50) {
-      throw new Error(
-        `MaxResults can be no greater than 50. You have requested ${maxResults}`,
-      );
-    }
+    if (options) {
+      const { maxResults } = options;
 
-    for (const key in options) {
-      if (options.hasOwnProperty(key)) {
-        const value = options[key as keyof typeof options];
+      if (maxResults && maxResults > 50) {
+        throw new Error(
+          `MaxResults can be no greater than 50. You have requested ${maxResults}`,
+        );
+      }
 
-        url += value ? `&${key}=${String(value)}` : '';
+      for (const key in options) {
+        if (options.hasOwnProperty(key)) {
+          const value = options[key as keyof typeof options];
+
+          url += value ? `&${key}=${String(value)}` : '';
+        }
       }
     }
+
+    return `${url}&key=${auth}`;
   }
 
-  return `${url}&key=${auth}`;
+  return generateEndpointUrl;
+  /* 
+    The returned function generateEndpointUrl has closure over the auth variable. 
+    This is a contrived example since the generated endpoint appends the api key 
+    to the end of the URL for the whole world to see. However if this was an 
+    auth token for authorization to perform CRUD operations on a database, it 
+    would be a wise pattern to follow as not expose sensitive data and being 
+    vulnerable to bad actor attacks. 
+
+    Here it servers two purposes:
+
+    1. To demonstrate that I (Tom) understands how closures work in JavaScript.
+    2. A placeholder for when this app grows! 
+  */
 }
 
-export const getYouTubeChannelsEndpoint = (options: ChannelsOptions) =>
-  getYouTubeApiEndpoint('channel', options);
+/* 
+  The type property defines which API methods are compatible with each other.
+  We use TypeScript to prevent incorrect configuration and failed API requests.
+*/
+export function getYouTubeChannelsEndpoint(options: ChannelsOptions) {
+  const generateEndpointUrl = getEndpointGenerator();
+  return generateEndpointUrl('channel', options);
+}
 
-export const getYouTubeVideosEndpoint = (options: VideoOptions) =>
-  getYouTubeApiEndpoint('video', options);
+export function getYouTubeVideosEndpoint(options: VideoOptions) {
+  const generateEndpointUrl = getEndpointGenerator();
+  return generateEndpointUrl('video', options);
+}
